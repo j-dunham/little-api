99e88aa46ae87072fce82b1231337d4828fef79b
j-dunham <jmdunham20@gmail.com>
2021-02-21 20:34:02 -0500

    Add Whitenoise for static file handling

--- slow-api/api.py
+++ slow-api/api.py
@@ -6,18 +6,23 @@ from jinja2 import Environment, FileSystemLoader
 from parse import parse
 from requests import Session as RequestsSession
 from webob import Request, Response
+from whitenoise import WhiteNoise
 from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
 
 
 class API:
-    def __init__(self, templates_dir="templates"):
+    def __init__(self, templates_dir="templates", static_dir="static"):
         self.routes = {}
         self.exception_handler = None
         self.templates_env = Environment(
             loader=FileSystemLoader(os.path.abspath(templates_dir))
         )
+        self.white_noise = WhiteNoise(self.wsgi_app, root=static_dir)
 
     def __call__(self, environ: Dict, start_response: Callable) -> Iterator:
+        return self.white_noise(environ, start_response)
+
+    def wsgi_app(self, environ, start_response) -> Iterator:
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
