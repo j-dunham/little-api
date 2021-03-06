d0f434ff1027b17278d922c4b5d1a735f7f11ede
j-dunham <jmdunham20@gmail.com>
2021-03-06 08:32:44 -0500

    Added table create

--- little_api/orm.py
+++ little_api/orm.py
@@ -1,3 +1,4 @@
+import inspect
 import sqlite3
 
 SQLITE_TYPE_MAP = {
@@ -13,13 +14,30 @@ class Database:
     def __init__(self, path):
         self.conn = sqlite3.Connection(path)
 
+    def create(self, table):
+        self.conn.execute(table._get_create_sql())
+
     @property
     def tables(self):
-        return []
+        rows = self.conn.execute(
+            "SELECT name FROM sqlite_master WHERE type = 'table';"
+        ).fetchall()
+        return [row[0] for row in rows]
 
 
 class Table:
-    pass
+    @classmethod
+    def _get_create_sql(cls):
+        create_sql = "CREATE TABLE IF NOT EXISTS {name} ({fields});"
+        fields = ["id INTEGER PRIMARY KEY AUTOINCREMENT"]
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(f"{name} {field.sql_type}")
+            elif isinstance(field, ForeignKey):
+                fields.append(f"{name}_id INTEGER")
+        fields = ", ".join(fields)
+        name = cls.__name__.lower()
+        return create_sql.format(name=name, fields=fields)
 
 
 class Column:
--- tests/test_orm.py
+++ tests/test_orm.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 
 import pytest
@@ -6,12 +7,21 @@ from little_api.orm import Column, Database, ForeignKey, Table
 
 
 @pytest.fixture
+def db(path: str = "./test.db"):
+    # Remove database if exists
+    if os.path.exists(path):
+        os.remove(path)
+    db = Database(path)
+    yield db
+
+
+@pytest.fixture
 def Author():
     class Author(Table):
         name = Column(str)
         age = Column(int)
 
-    return Author
+    yield Author
 
 
 @pytest.fixture
@@ -21,11 +31,10 @@ def Book(Author):
         published = Column(bool)
         author = ForeignKey(Author)
 
-    return Book
+    yield Book
 
 
-def test_create_db():
-    db = Database("./test.db")
+def test_create_db(db):
     assert isinstance(db.conn, sqlite3.Connection)
     assert db.tables == []
 
@@ -36,3 +45,22 @@ def test_table_create(Author, Book):
 
     assert Author.name.sql_type == "TEXT"
     assert Author.age.sql_type == "INTEGER"
+
+
+def test_create_table(db, Author, Book):
+
+    db.create(Author)
+    db.create(Book)
+
+    assert (
+        Author._get_create_sql() == "CREATE TABLE IF NOT EXISTS author (id INTEGER "
+        "PRIMARY KEY AUTOINCREMENT, age INTEGER, "
+        "name TEXT);"
+    )
+    assert (
+        Book._get_create_sql() == "CREATE TABLE IF NOT EXISTS book (id INTEGER "
+        "PRIMARY KEY AUTOINCREMENT, author_id INTEGER, "
+        "published INTEGER, title TEXT);"
+    )
+    for table in ("author", "book"):
+        assert table in db.tables
